50a6ab5577a591d600b27c558d232eaf892e4c32
Theocode12 <vchidubem9@gmail.com>
2023-01-09 22:18:36 +0100

    sorting coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """A module to illustrate the asynchronous io"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays (float values)"""
     return sorted(await asyncio.gather(*(wait_random(max_delay)
                                          for _ in range(n))))
