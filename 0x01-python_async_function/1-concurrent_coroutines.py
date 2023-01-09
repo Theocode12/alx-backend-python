a277433be1ec422bf49d5568a4225924ebfe7933
Theocode12 <vchidubem9@gmail.com>
2023-01-09 21:24:52 +0100

    annotations fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """A module to illustrate the asynchronous io"""
 
-from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> list:
     """return the list of all the delays (float values)"""
     return [await wait_random(max_delay) for i in range(n)]
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-from typing import List
+"""A module to illustrate the asynchronous io"""
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> list:
     """return the list of all the delays (float values)"""
     return [await task_wait_random(max_delay) for i in range(n)]
