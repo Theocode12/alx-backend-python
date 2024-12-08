bd8b19e5a7c6733ea381bafa7f7938e38eb2dd7d
Theocode12 <vchidubem9@gmail.com>
2024-12-08 12:32:33 +0100

    dataime fix that i dont think is needed

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,5 +1,13 @@
 import sqlite3
 import functools
+from datetime import datetime
+import logging
+
+# Proper logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 #### decorator to lof SQL queries
 
@@ -8,8 +16,10 @@ import functools
 def log_queries(func):
    @functools.wraps(func)
    def wrapper(*args, **kwargs):
-      print(kwargs.get('query'))
-      return func(*args, **kwargs)
+        query = kwargs.get("query")
+        if query:
+            logging.info(f"Executing query: {query}")
+        return func(*args, **kwargs)
    return wrapper
 
 @log_queries
@@ -23,25 +33,3 @@ def fetch_all_users(query):
 
 #### fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
-print(users)
-
-from sqlite3 import Connection
-## create user
-def connect(func):
-    def wrapper(arg):
-        conn = sqlite3.connect('users.db')
-        func(arg, conn)
-        result = conn.execute("SELECT * FROM users")
-        print(result.fetchone())
-        conn.commit()
-        conn.close()
-    return wrapper
-
-@connect
-def db_query(query, connection: Connection):
-    connection.cursor().execute(query)
-    # pass
-
-# db_query("CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY , name VARCHAR(40), email VARCHAR(40) UNIQUE, age INT)")
-
-# db_query("""INSERT INTO users (name, age, email)VALUES ('Alice', 30, 'Bob@example.com')""")
