7f9ccde7dab36261daea4a6c0341fd4003be0f07
Theocode12 <vchidubem9@gmail.com>
2024-12-12 11:20:20 +0100

    context managers and async

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,15 +5,16 @@ import asyncio
 async def async_fetch_users():
     async with aiosqlite.connect('users.db') as db:
         cursor = await db.execute("SELECT * FROM users")
-        print(await cursor.fetchall())
+        return await cursor.fetchall()
 
 async def async_fetch_older_users():
     async with aiosqlite.connect('users.db') as db:
         cursor = await db.execute("SELECT * FROM users WHERE age > 40")
-        print(await cursor.fetchall())
+        return await cursor.fetchall()
 
 async def fetch_concurrently():
-    await asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    return await asyncio.gather(async_fetch_users(), async_fetch_older_users())
 
 if __name__ == "__main__":
-     asyncio.run(fetch_concurrently())
+    results = asyncio.run(fetch_concurrently())
+    print(results)
