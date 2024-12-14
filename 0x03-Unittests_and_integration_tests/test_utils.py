b2ccc3ac7e098b4b234adf2c5f1c47019756a4d6
Theocode12 <vchidubem9@gmail.com>
2024-12-14 21:38:33 +0000

    add test for access_nested_map for invalid path

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,12 +14,30 @@ from typing import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Tests utils.access_nested_map
-    """
-    @parameterized.expand([
-        ("gets int 1", {"a": 1}, ("a",), 1),
-        ("gets dict - {b: 2}", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("gets int  2", {"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, name: str, nested_map: Mapping, keys: Sequence, value: Any):
+    """Tests utils.access_nested_map"""
+
+    @parameterized.expand(
+        [
+            ("gets int 1", {"a": 1}, ("a",), 1),
+            ("gets dict - {b: 2}", {"a": {"b": 2}}, ("a",), {"b": 2}),
+            ("gets int  2", {"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(
+        self, name: str, nested_map: Mapping, keys: Sequence, value: Any
+    ):
+        """tests valid path"""
         self.assertEqual(access_nested_map(nested_map, keys), value)
+
+    @parameterized.expand(
+        [
+            ("attempts to access empty map", {}, ("a",)),
+            ("attempts to access a non existent map", {"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(
+        self, name: str, nested_map: Mapping, keys: Sequence
+    ):
+        """tests invalid path"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, keys)
