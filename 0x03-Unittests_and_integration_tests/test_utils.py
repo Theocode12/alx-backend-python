886ea7582296613e5c069c60b7058623b555b79d
Theocode12 <vchidubem9@gmail.com>
2024-12-15 11:33:34 +0000

    rm name in parameterized input

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,13 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand(
         [
-            ("gets int 1", {"a": 1}, ("a",), 1),
-            ("gets dict - {b: 2}", {"a": {"b": 2}}, ("a",), {"b": 2}),
-            ("gets int  2", {"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
     def test_access_nested_map(
-        self, name: str, nested_map: Mapping, keys: Sequence, value: Any
+        self, nested_map: Mapping, keys: Sequence, value: Any
     ):
         """tests valid path"""
         self.assertEqual(access_nested_map(nested_map, keys), value)
