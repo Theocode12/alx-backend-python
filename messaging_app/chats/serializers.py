3beeedcd69347d76de031291ec027f0e124f8e8e
Theocode12 <vchidubem9@gmail.com>
2024-12-25 19:21:32 +0000

    add validation and serializerMethod

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -11,8 +11,19 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = ['message_id', 'sender_id', 'receiver_id', 'message_body', 'sent_at', 'conversation_id']
 
 class ConversationSerializer(serializers.ModelSerializer):
-    message_set = MessageSerializer(many=True)
+    message_set = serializers.SerializerMethodField()
     participants = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, pk_field=serializers.UUIDField(format='hex'))
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'created_at', 'message_set', 'participants']
+   
+    def get_message_set(self, obj):
+        messages = Message.objects.all()
+        filtered_messages = messages.filter(conversation_id=obj.id)
+        serialized_messages = MessageSerializer(filtered_messages, many=True)
+        return serialized_messages.data
+
+    def validate_participants(self, value):
+        if len(participants) != 2:
+            raise serializers.ValidationError("participants must be 2 or more")
+        return value
