88d82c05400dc9f598acf26568923327072820dd
Theocode12 <vchidubem9@gmail.com>
2024-12-25 01:38:29 +0100

    pd fix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from chats.views import MessageViewSet, ConversationViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 
-routers = routers.DefaultRouter()
-routers.register('messages', MessageViewSet)
-routers.register('conversations', ConversationViewSet)
+conv_router = DefaultRouter()
+conv_router.register(r'conversations', ConversationViewSet)
+
+message_router = NestedDefaultRouter(conv_router, r'conversations', 'conversations')
+message_router.register('messages', MessageViewSet)
 
 urlpatterns = [
-    path('', include(routers.urls)),
+    path('', include(conv_router.urls)),
+    path('', include(message_router.urls)),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
