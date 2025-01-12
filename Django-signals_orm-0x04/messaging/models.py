b3660fa20b33befac3e2c97c360db4cab3cb8ae0
Maduagwu Chidubem Valentine <valentine@Theo.>
2025-01-12 08:58:17 +0100

    notification fix

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,7 +7,15 @@ class Message(models.Model):
     receiver = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
+    edited = models.BooleanField()
 
 class Notification(models.Model):
     receiver = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
     message = models.ForeignKey('Message', on_delete=models.CASCADE, db_column='message_id')
+
+class MessageHistory(models.Model):
+    sender = models.ForeignKey('User', on_delete=models.CASCADE, db_column='sender_id')
+    receiver = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    edited = models.BooleanField()
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,12 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import Notification, Message
+from django.db.models.signals import post_save, pre_save
+from .models import Notification, Message, MessageHistory
 
 @receiver(post_save, sender=Message)
-def notify(sender, **kwargs):
+def notify(sender: Message, **kwargs):
     Notification.objects.create(receiver=sender.receiver, message=sender)
+
+
+@receiver(pre_save, sender=Message)
+def loggingMessageHistory(sender: Message, **kwargs):
+    MessageHistory.objects.create(sender=sender.sender, receiver=sender.receiver, content=sender.content, edited=sender.edited, timestamp=sender.timestamp)
