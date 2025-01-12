1b417eb63bdfb9c7d6cf8d721d0d3a0dfdda02dd
Maduagwu Chidubem Valentine <valentine@Theo.>
2025-01-12 08:34:05 +0100

    reciever fix

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Message(models.Model):
     sender = models.ForeignKey('User', on_delete=models.CASCADE, db_column='sender_id')
-    reciever = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
+    receiver = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
 
 class Notification(models.Model):
-    reciever = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
+    receiver = models.ForeignKey('User', on_delete=models.CASCADE, db_column='reciever_id')
     message = models.ForeignKey('Message', on_delete=models.CASCADE, db_column='message_id')
